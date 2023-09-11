6fe31b8214520a076d00efaa43ec151fe942b802
jasimahmedq2 <jasimahmed90852417@gmail.com>
2023-09-11 21:07:54 +0600

    academic bottom part done

--- src/pages/academics/academics.jsx
+++ src/pages/academics/academics.jsx
@@ -21,6 +21,16 @@ const Academics = () => {
       <div>
         <AcademicCard />
       </div>
+      <div className="text-center space-y-4">
+        <h3 className="text-xl font-bold text-white">ATHLETICS</h3>
+        <p className="text-white ">
+          Contact us to learn more about our sports program and opportunities
+          for your child.
+        </p>
+        <button className="text-white border border-white rounded-full px-4 py-1">
+          Athletics info
+        </button>
+      </div>
     </div>
   );
 };
