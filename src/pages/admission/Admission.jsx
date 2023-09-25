493a28573ade58d68880b4e0ae497afb57ba77b8
Mamun2k22 <mamun180037@gmail.com>
2023-09-26 00:55:01 +0600

    update nav

--- src/pages/admission/Admission.jsx
+++ src/pages/admission/Admission.jsx
@@ -1,11 +1,13 @@
-import AdmissionPage from './admissionPage';
+
 import ApplicationProcess from './ApplicationProcess';
 import AnnulTution from './AnnulTution';
 import Financial from './Financial';
+import AdmissionBanner from './AdmissionBanner';
 
 const Admission = () => {
     return (
         <div>
+            <AdmissionBanner></AdmissionBanner>
             <ApplicationProcess></ApplicationProcess>
             <AnnulTution></AnnulTution>
             <Financial></Financial>
--- src/pages/admission/Financial.jsx
+++ src/pages/admission/Financial.jsx
@@ -47,7 +47,7 @@ const Financial = () => {
           </div>
         </div>
        
-      </div>
+        </div>
         </div>
     );
 };
