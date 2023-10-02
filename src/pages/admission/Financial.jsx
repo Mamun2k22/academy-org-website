a302f4a0afe47ddaeda6056ce165ff82deab0dc8
Mamun2k22 <mamun180037@gmail.com>
2023-10-02 10:17:53 +0600

    fix the admission

--- src/pages/admission/Financial.jsx
+++ src/pages/admission/Financial.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 
 const Financial = () => {
     return (
-        <div className=''>
+        <div className='lg:mt-10'>
 
         <div
-        className="bg-cover bg-center h-screen lg:my-20 relative bg-fixed"
+        className="bg-cover bg-center h-screen lg:my-0 relative bg-fixed"
         style={{
           backgroundImage:
             'url("https://libertylaunchacademy.org/wp-content/uploads/2023/07/LibertyLaunchAcademy_K-12_PrivateSchool-LibertyLake-WA-IG-23.jpeg")',
